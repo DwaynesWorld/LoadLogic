4be48c66d8b842e4e5c127826ebc75b529164358
Kyle Thompson <kthompson@713@gmail.com>
2020-08-30 17:15:18 -0500

    initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,147 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import React from "react";
+import styled from "styled-components";
+import logo from "./logo.png";
+import { Switch, Route, Link, NavLink, Redirect } from "react-router-dom";
+import { Navbar, Nav as RBNav, NavDropdown } from "react-bootstrap";
+import { MdSearch, MdNotifications, MdSettings } from "react-icons/md";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="logo"
+          />
+        </Navbar.Brand>
+
+        <Nav>
+          <PrimaryNav>
+            <Nav.Link as={NavLink} to="/dashboard">
+              Dashboard
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/orders">
+              Orders
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/billing">
+              Billing
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/vendors">
+              Vendors
+            </Nav.Link>
+          </PrimaryNav>
+
+          <SecondaryNav>
+            <Nav.Link>
+              <MdSearch size={22} />
+            </Nav.Link>
+
+            <Nav.Link>
+              <MdNotifications size={22} />
+            </Nav.Link>
+
+            <Nav.Link as={NavLink} to="/settings">
+              <MdSettings size={22} />
+            </Nav.Link>
+
+            <NavDropdown id="basic-nav-dropdown" title="Hello, John">
+              <NavDropdown.Item href="#action/3.1">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </SecondaryNav>
+        </Nav>
+      </Navbar>
+
+      <div>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/orders">
+            <Orders />
+          </Route>
+          <Route path="/billing">
+            <Billing />
+          </Route>
+          <Route path="/vendors">
+            <Vendors />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+          <Route path="/">
+            <Redirect to="/dashboard" />
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
+
+function Orders() {
+  return <h2>Orders</h2>;
+}
+
+function Billing() {
+  return <h2>Invoices</h2>;
+}
+
+function Vendors() {
+  return <h2>Vendors</h2>;
+}
+
+function Settings() {
+  return <h2>Settings</h2>;
+}
+
+const Nav = styled(RBNav)`
+  flex: 1;
+  justify-content: flex-end;
+`;
+
+const PrimaryNav = styled.div`
+  display: flex;
+
+  a.nav-link {
+    display: flex;
+    align-items: center;
+    text-transform: uppercase;
+    font-size: 14px;
+    font-weight: 600;
+    padding-left: 15px !important;
+  }
+`;
+
+const SecondaryNav = styled.div`
+  display: flex;
+  margin-left: 20px;
+  padding-left: 20px;
+  border-left: 1px solid rgba(255, 255, 255, 0.25);
+
+  .dropdown-menu {
+    right: 0;
+    left: auto;
+  }
+`;
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./index.css";
+import * as serviceWorker from "./service-worker";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Root() {
+  return (
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.render(<Root />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
