57d22e924c4aa4deaaa3d2cc7b74ae07966756ba
Kyle Thompson <kthompson@713@gmail.com>
2020-09-01 08:39:46 -0500

    cleanup sln and setup

--- src/Services/Ordering/Ordering.EventHub/Startup.cs
+++ src/Services/Ordering/Ordering.EventHub/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MassTransit;
+
 namespace Ordering.EventHub
 {
     public class Startup
@@ -24,7 +26,6 @@ namespace Ordering.EventHub
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
         }
 
@@ -39,7 +40,6 @@ namespace Ordering.EventHub
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
